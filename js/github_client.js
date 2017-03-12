3b80077d0f6f7dc7a3603adb4e7e11eeffd79edc
fatmaMY <fatima.mohammad736@gmail.com>
2017-03-12 22:17:32 +0300

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,59 +1,53 @@
 //define functions here
 var createGist = function(file_name, content, description, token){
-  var data = {
-    'public':   true,
-    'description': description,
-    'files': {}
-  };
+	var data = {
+		'public':   true,
+		'description': description,
+		'files': {}
+	};
 
-  data['files'][file_name] = {
-    'content': content
-  };
+	data['files'][file_name] = {
+		'content': content
+	};
 
-  $.ajax({
-    url: 'https://api.github.com/gists',
-    type: 'POST',
-    dataType: 'json',
-    headers: {
+	$.ajax({
+		url: 'https://api.github.com/gists',
+		type: 'POST',
+		dataType: 'json',
+		headers: {
 			Authorization: "token "+token,
 		},
-   
-    data: JSON.stringify(data)
-  }).done(function(response) {
-    myGists(response.owner.login, token);
-  });
+		
+		data: JSON.stringify(data)
+	}).done(function(response) {
+		myGists(username, token);
+	});
 };
 
 var myGists = function (username, token){
-$.ajax({
-  url: 'https://api.github.com/gists/'+gists,
-  type: 'GET',
-  dataType: 'jsonp'
-}).success( function(gistdata) {
-    var content = gistdata.data.files[filename].content;
-    $.each(gists.data, function(index, gist) {
-      var link = $('<a>')
-        .attr('href', gist.html_url)
-        .text(gist.description);
+	$.ajax({
+		url: 'https://api.github.com/users/'+username+'/'+token,
+		type: 'GET',
+		dataType: 'jsonp'
+	}).success( function(gistdata) {
+		
+			var link = `<a href = '${gistdata.html_url}'>${gistdata.description}</a>`;
 
-      var listItem = $('<li>')
-        .append(link);
-
-      $('#myGists').append(listItem);
-    });
-  });
+			$('#myGists').append(link);
+		});
 }
 
 
 var bindCreateButton = function() {
-  $('#create').click(function() {
-    var token = $('#token').val();
-    var file_name = $('#file_name').val();
-    var content = $('#content').val();
-    var description = $('#description').val();
+	$('#form').submit(function(e) {
+        e.preventDefault();
+		var token = $('#token').val();
+		var file_name = $('#file_name').val();
+		var content = $('#content').val();
+		var description = $('#description').val();
 
-    createGist(file_name, content, description, token);
-  });
+		createGist(file_name, content, description, token);
+	});
 };
 
 $(document).ready(function(){
